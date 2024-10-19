85bb2704724257bb603ee647ff5e469e49ce94dc
seif2001 <seifelshabshiri@aucegypt.edu>
2024-10-19 22:31:49 +0300

    made small server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::net::UdpSocket;
+
 fn main() {
-    println!("Hello, world!");
+    let socket = UdpSocket::bind("0.0.0.0:4000").unwrap();
+
+    println!("Listening on {}", socket.local_addr().unwrap());
+
+    let mut buff = [0; 1024];
+
+    loop {
+        let (size, source) = socket.recv_from(&mut buff).unwrap();
+
+        let request = String::from_utf8_lossy(&buff[..size]);
+
+        println!("Recieved {} from {}", request, source);
+
+        let response = request;
+
+        socket.send_to(response.as_bytes(), source).unwrap();
+    }
 }
