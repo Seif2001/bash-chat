b278196c3eda44fb930612a6decd1a52fb7b9312
seif2001 <seifelshabshiri@aucegypt.edu>
2024-12-01 08:02:52 +0200

    fixed merge issues

--- src/bin/client/main.rs
+++ src/bin/client/main.rs
@@ -53,7 +53,23 @@ async fn main() -> io::Result<()> {
     // // image_com::send_images_from_to(&config.client_raw_images_dir, 1, 1, Ipv4Addr::new(10, 7, 16, 43), config.port_client_rx, &socket_arc, &config).await?;
     dos::register_dos(&socket_arc, &config_arc).await?;
     dos::request_dos(&socket_arc, &config_arc).await?;
-
+    let socket_arc_clone = Arc::clone(&socket_arc);
+    let config_clone = Arc::clone(&config_arc);
+    let json_path = "image_requests_unfinished.json";
+    let requests = match image_processor::read_image_requests(json_path) {
+        Ok(requests) => requests,
+        Err(e) => {
+            eprintln!("Failed to read or parse the JSON file: {}", e);
+            return Err(e);
+        }
+    };
+    for request in requests {
+        // Assuming `request_image` is the function to send a request
+        println!("Making the request for image: {}", request.image_name);
+        let sending_socket = socket_arc.new_client_socket().await;
+        let client_ip: Ipv4Addr = dos::get_ip_by_username_as_ipv4(&request.client_username)?;
+        let _ = api::request_image(&socket_arc, &config_clone, sending_socket, request.image_name, client_ip, config_clone.port_client_image_request_rx, request.is_high).await;
+    }
     let socket_arc_clone = Arc::clone(&socket_arc);
     let config_clone = Arc::clone(&config_arc);
     let _ = tokio::spawn({
@@ -61,47 +77,8 @@ async fn main() -> io::Result<()> {
             let _ = middleware::p2p_recv_request(&socket_arc_clone, &config_clone).await;
         }
     });
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
 
     
->>>>>>> frontend
-    // dos::request_dos(&socket_arc, &config).await?;
-    // // let leader_ip: Ipv4Addr = middleware::recv_leader(&socket_arc, &config).await;
-    // // println!("Leader is {} ", leader_ip);
-    // // println!("Before send images");
-    // // image_com::send_images_to_server(&config.client_raw_images_dir, 1, 1, leader_ip, config.port_client_rx, &socket_arc, &config).await?;
-    // // println!("After send images");
-    // loop{}
-    //         // middleware::send_cloud(&socket, &config,&"START".to_string()).await?;
-=======
-    let sending_socket = socket_arc.new_client_socket().await;
-    let image_name = "image3.png";
-    // let client_ip: Ipv4Addr = dos::get_ip_by_username_as_ipv4(&"yehiaz")?;
-    let client_port = config.port_client_image_request_rx;
-     let _ = api::request_update_views(&socket_arc, &config, sending_socket, image_name.to_string(), client_port,100).await;
-        
-           loop{}
->>>>>>> d4b0603ee92659e872e8f3971b5e5b8ab79c34cb
-
-    //     // let mut clients = dos::parse_clients("clients_request.json",&config.username);
-    //     // dos::print_clients(clients);
-    //     // image_store::create_json_for_images(&config.client_raw_images_dir, "my_images.json").unwrap();
-    //     // let client_ip = Ipv4Addr::new(10, 7, 16, 43);
-    //     // api::request_list_images(&socket, &config, client_ip).await?;
-    
-    //     // Client 2 Config
-    //     // Respond to "image Request"
-    //     // middleware::p2p_recv_image_request(&socket, &config).await?;
-    //      let sending_socket = socket.new_client_socket().await;
-    //      let image_name = "image3.png";
-    //     // let client_ip: Ipv4Addr = Ipv4Addr::new(10, 7, 19, 101);
-    //     let client_ip: Ipv4Addr = dos::get_ip_by_username_as_ipv4(&"yehia")?;
-    //     let client_port = config.port_client_image_request_rx;
-    //     let _ = api::request_image(&socket, &config, sending_socket, image_name.to_string(), client_ip, client_port, false).await;
-    //     let high_path = Path::new(&config.client_high_quality_receive_dir).join(&image_name);
-    //     image_processor::display_image(&high_path.display().to_string());
     let config = Config::new();
     let socket = socket_arc.clone();
     frontend::run(&*socket, config).await;
